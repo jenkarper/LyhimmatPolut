94340f62abb0cf236c0c50248cc65c75f29d9ed2
jenkarper <55617257+jenkarper@users.noreply.github.com>
2021-03-17 20:48:09 +0200

    Move project

---
+++ src/main/java/lyhimmatpolut/Main.java
@@ -0,0 +1,13 @@
+package lyhimmatpolut;
+
+import ui.GUI;
+
+/**
+ *
+ * @author pertjenn
+ */
+public class Main {
+    public static void main(String[] args) {
+        GUI.main(args);
+    }
+}
---
+++ src/main/java/ui/GUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author pertjenn
+ */
+public class GUI extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Dijkstra!");
+    }
+}
